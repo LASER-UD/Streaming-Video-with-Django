adfcaa00cef652bcb70c84a90e929b2bca2cf035
jeisongarcia <jegarciat@correo.udistrital.edu.co>
2019-01-24 17:15:24 -0500

    Se trasmite video webcam para varios usuarios y se detectan las teclas oprimida por el clinetes

---
+++ eventos_teclado/eventos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='eventos'
+urlpatterns=[
+   path('',views.index,name='index'),
+   path('tecla_pre/',views.tecla_pre,name='tecla_pre'),
+   path('tecla_sol/',views.tecla_sol,name='tecla_sol'),
+   path('video/',views.video,name='video'),
+]
---
+++ eventos_teclado/eventos/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+import cv2
+import threading
+from django.views.decorators.gzip import gzip_page
+from django.http import StreamingHttpResponse
+# Create your views here.
+def index(request):
+    x= 12
+    direccion = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhB82XwnxSmPIer3V_bStkR-RqxlhFFcIc7GKoxX7NUXHt0N0S'
+    return render(request,'eventos/index.html',{'person': x,'imagen': direccion})
+
+#@csrf_exempt
+
+
+def tecla_pre(request):
+	if request.method == 'POST':
+		
+		tecla = request.POST.get('tecla_pre')
+		print(tecla)
+		response_data = {}
+		response_data['tecla'] = tecla
+		response_data['accion'] = 'presionada'
+		return HttpResponse(json.dumps(response_data), content_type="application/json")
+		
+	else:
+        	return HttpResponse(
+            	json.dumps({"nothing to see": "this isn't happening"}),
+            	content_type="application/json"
+       		)
+
+def tecla_sol(request):
+	if request.method == 'POST':
+		
+		tecla = str(chr(int(request.POST.get('tecla_sol'))))
+		print(tecla)
+		response_data = {}
+		response_data['tecla'] = tecla
+		response_data['accion'] = 'soltada'
+		return HttpResponse(json.dumps(response_data), content_type="application/json")
+		
+	else:
+        	return HttpResponse(
+            	json.dumps({"nothing to see": "this isn't happening"}),
+            	content_type="application/json"
+       		)
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+camera=VideoCamera()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip_page
+def video(request):
+  
+    return StreamingHttpResponse(gen(camera), content_type="multipart/x-mixed-replace;boundary=frame")
+    
+
