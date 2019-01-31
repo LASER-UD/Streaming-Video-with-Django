3e2be6d2201b6625adc02589c5072960495b580c
nickdpz <ndpastranzamora@gmail.com>
2019-01-30 22:38:08 -0500

    Se añade la señal de vida y codigo para ejecutar script desde el arranque con instrucciones en el Readme

---
+++ SignalofLive.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+
+response = 1
+while response==1:
+	response=os.system("ping -c 1 google.com &")
+	sleep(1)
+
+os.system("python3 /home/pi/Documents/Streaming-Video-with-Django/eventos_teclado/manage.py runserver 0:8000 &")
+
+while True:
+	
+	response=os.system("ping -c 1 google.com &")
+
+	
+	if response==0:
+		GPIO.output(7,True)
+		sleep(0.5) 
+		GPIO.output(7,False)
+		sleep(0.5) 
+
+	sleep(1)
