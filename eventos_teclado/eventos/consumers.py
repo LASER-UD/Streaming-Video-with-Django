b76e7e0be84f69d516ce977e57fcc7403c44bdb7
jeisongarcia9713 <jegarciat@correo.udistrital.edu.co>
2019-03-17 23:49:57 -0500

    varios usuarios pueden ver la transmision y enviar datos al servidor

--- eventos_teclado/eventos/consumers.py
+++ eventos_teclado/eventos/consumers.py
@@ -6,16 +6,17 @@ import cv2
 import threading
 import time
 class VideoCamera(object):
+    clientes=0;
+    image=None;
     def start(self):
-        self.cliente = False
         self.video = cv2.VideoCapture(0)
         (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
 
     def end(self):
         self.video.release()
 
     def get_frame(self):
+        (self.grabbed, self.frame) = self.video.read()
         image = self.frame
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
@@ -24,11 +25,12 @@ class VideoCamera(object):
         while self.video.read():
             (self.grabbed, self.frame) = self.video.read()
 
+
 camera = VideoCamera()
+
 #sincrono
-class ChatConsumer(WebsocketConsumer):
-    frame=None
-    clientes=0
+class ChatConsumer1(WebsocketConsumer):
+    
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -38,72 +40,77 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
-        if(self.clientes==0):
-            camera.start()
-            self.clientes=self.clientes+1
-        else:
-            self.clientes=self.clientes+1
-        print(self.clientes)
+        camera.clientes=camera.clientes + 1
+        print(camera.clientes)
 
+        if(camera.clientes==1):
+            camera.start()
+        
+        self.send(text_data=json.dumps({
+                'type':'clientes',
+                'message': camera.clientes
+            }))
+        
     def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        
+        camera.clientes=camera.clientes -1
+        if(camera.clientes==0):
+            camera.end()
+            async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
-        )
-        self.clientes=self.clientes-1
-        if(self.clientes==0):
-            camera.end()
-        print(self.clientes)
+            )
+        else:
+            async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': 'desc',
+                'tipo': 'clientes'
+            }
+            )
+            
+        print(camera.clientes)
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        self.frame=base64.b64encode(camera.get_frame())
-        self.send(text_data=self.frame.decode('ascii'))
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json['message']
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-        # # Send message to room group
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
-       
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'tipo': 'mensaje',
+            }
+        )
 
 
-    # Receive message from room group
     def chat_message(self, event):
         message = event['message']
-
-        # Send message to WebSocket
-        if message=='1':
-            self.frame=base64.b64encode(camera.get_frame())
-            self.send(text_data=self.frame.decode('ascii'))
-        else:    
-            self.send(text_data=json.dumps({
-                'type':'mensaje',
-                'message': message
-            }))
-
-  
-
+        tipo = event['tipo']
+        if(message=='1'):
+            self.send(text_data=json.dumps({'type':'imagen','message':base64.b64encode(camera.get_frame()).decode('ascii')}))
+        else:
+            self.send(text_data=json.dumps({'type':tipo,'message':message}))
+            
+    
+       
 
 #asincrono
 #https://channels.readthedocs.io/en/latest/tutorial/part_3.html explicacion de como usar 
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer1(AsyncWebsocketConsumer):
-    frame=None
-    clientes=0
+class webcam(AsyncWebsocketConsumer):
+    
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        
+        self.room_group_name = 'webcam'
 
         # Join room group
         await self.channel_layer.group_add(
@@ -112,53 +119,92 @@ class ChatConsumer1(AsyncWebsocketConsumer):
         )
 
         await self.accept()
-        if(self.clientes==0):
-             self.clientes=self.clientes+1
-             threading.Thread(target=self.gen(camera), args=()).start()
-        else:
-             self.clientes=self.clientes+1
-             print(self.clientes)
+        camera.clientes=camera.clientes + 1
+        print(camera.clientes)
+
+        if(camera.clientes==1):
+            camera.start()
+        
+        await self.send(text_data=json.dumps({
+                'type':'clientes',
+                'message': camera.clientes
+            }))
+        
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
+        camera.clientes=camera.clientes -1
+        if(camera.clientes==0):
+            camera.end()
+            await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
-        )
-        self.clientes=self.clientes-1
-        print(self.clientes)
+            )
+        else:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': 'desc',
+                    'tipo': 'clientes'
+                }
+            )
+            
+        print(camera.clientes)
+        
 
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+        if text_data_json['tipo']=='tecla':
+            print(message)
+        else:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'tipo': 'mensaje'
+                }
+            )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        tipo = event['tipo']
+        if(message=='1'):
+            await self.send(text_data=json.dumps({'type':'imagen','message':base64.b64encode(camera.get_frame()).decode('ascii')}))
+        else:
+            await self.send(text_data=json.dumps({'type':tipo,'message':message}))
+   
+class tecla(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        
+        self.room_group_name = 'tecla'
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-             'type':'mensaje',
-            'message': message
-        }))
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def gen(self,camera):
-        camera.start();
-        while True:
-            if self.clientes>0:
-                self.frame =base64.b64encode(camera.get_frame())
-                self.send(text_data=self.frame)
-            else:
-                camera.end()
-                break
+        await self.accept()
+        
+        
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print(message)
--- eventos_teclado/eventos/routing.py
+++ eventos_teclado/eventos/routing.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/eventos/chat/<slug:room_name>/', consumers.ChatConsumer),
+    path('ws/eventos/video/', consumers.webcam),
+    path('ws/eventos/tecla/', consumers.tecla),
 ]
